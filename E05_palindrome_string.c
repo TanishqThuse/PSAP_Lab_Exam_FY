3d9d5a1a492ccbcb5a8a2e615f73ac7d1832fd1c
TanishqThuse <tanishqthuse555@gmail.com>
2024-05-17 16:35:36 +0530

    Q5

--- E05_palindrome_string.c
+++ E05_palindrome_string.c
@@ -11,4 +11,36 @@ Output: false*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<stdbool.h>>
+#include<stdbool.h>
+
+int main(){
+    char str[100];
+    bool flag = false;
+    //Taking input
+    printf("Enter a string: ");
+    scanf("%s", &str);
+
+    //Finding length of string
+    int l = strlen(str);
+
+    //Checking if it is palindrome
+    for(int i=0; str[i] != '\0' ;i++){
+        if(str[i] != str[l-i-1]){
+            flag = false;
+            // printf("true\n");
+        }
+        else{
+            flag = true;
+        }
+    }
+    if(flag == false){
+        printf("false\n");
+    }
+    else{
+        printf("true\n");
+    }
+
+    return 0;
+
+}
+
