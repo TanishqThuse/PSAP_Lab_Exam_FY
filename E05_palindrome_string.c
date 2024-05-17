91bf5cc0656e0db967e27f4c30835c472c0e3de2
TanishqThuse <tanishqthuse555@gmail.com>
2024-05-17 16:08:25 +0530

    Updated Q1

---
+++ E05_palindrome_string.c
@@ -0,0 +1,14 @@
+/*E05
+Given a string s, return true if it a Palindrome or false otherwise.
+Test Cases:
+Case 1:
+Input: MADAM
+Output: true
+Case 2:
+Input: CAR
+Output: false*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>>
