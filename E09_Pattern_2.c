f88c07b1caf3bad3703a5448b6d5e869a1fd8644
TanishqThuse <tanishq.thuse23@vit.edu>
2024-05-17 18:30:29 +0530

    Q9

--- E09_Pattern_2.c
+++ E09_Pattern_2.c
@@ -7,8 +7,17 @@
 
 int main(){
     for(int i=1; i<=5; i++){
-        for(int j=1; j<i;j++){
+        for(int j=1; j<=i;j++){
             printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    for(int k=1; k<=5; k++){
+    char ch = 'E';
+        for(int l=5; l>=k; l--){
+            printf("%c ",ch);
+            ch--;
         }
         printf("\n");
     }
