29807034bb0791f935610b816f7114e407292c99
TanishqThuse <tanishq.thuse23@vit.edu>
2024-05-19 13:38:56 +0530

    Q35_q2

--- E35_q2_max_num_in_arr.c
+++ E35_q2_max_num_in_arr.c
@@ -3,6 +3,26 @@ Max num in arr using func*/
 
 #include<stdio.h>
 
+int max(int a[5]){
+    int max = a[0];
+    for(int i=0 ; i<5 ; i++){
+        if(a[i]>max){
+            max = a[i];
+        }
+    }
+    return max;
+}
+
+int min(int a[5]){
+    int min = a[0];
+    for(int i=0 ; i<5 ; i++){
+        if(a[i]<min){
+            min = a[i];
+        }
+    }
+    return min;
+}
+
 int main(){
 
     int a[5];
@@ -12,19 +32,9 @@ int main(){
     for(int i=0 ; i<5 ; i++){
         scanf("%d",&a[i]);
     }
-    int min = a[0];
-    int max = a[0];
+     
+    printf("max = %d\n",max(a));
 
-    for(int i=0; i<5; i++){
-        if(min>a[i]){
-            min=a[i];
-        }
-        if(max < a[i]){
-            max = a[i];
-        }
-    }  
-    printf("max = %d\n",max);
-
-    printf("min = %d\n",min);
+    printf("min = %d\n",min(a));
     return 0;
 }
